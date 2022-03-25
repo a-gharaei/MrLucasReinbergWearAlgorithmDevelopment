2533627e50c3d5292034c96fb7636a2820b1b7e6
lreinberg <lucasreinberg@outlook.com>
2022-03-25 10:02:57 +0100

    small changes

--- Main.py
+++ Main.py
@@ -23,26 +23,16 @@ grain = grain.Grain2D(a)
 # set initial values depending on the forces and the penetration depth
 grain.initializeValues(F_c, F_n, d)
 
+# Simulation
 initial_time = time.time()
 grain.make_crack(F_c, F_n)
 time1 = time.time()
 print(time1-initial_time)
 
-
-
 # Plotting
-fig, axs = plt.subplots(2, 2)
+fig, axs = plt.subplots()
 fig.suptitle('Original and modified grain')
-x1, y1 = hlp.polygon_to_plot(grain.vertices[0])
-x2, y2 = hlp.polygon_to_plot(grain.vertices[2])
-x3, y3 = hlp.polygon_to_plot(grain.vertices[-2])
-x4, y4 = hlp.polygon_to_plot(grain.vertices[-1])
-axs[0, 0].plot(x1, y1)
-axs[0, 0].set_title('Original grain')
-axs[0, 1].plot(x2, y2)
-axs[0, 1].set_title('Grain after first simulation step')
-axs[1, 0].plot(x3, y3)
-axs[1, 0].set_title('Grain before macro fracture')
-axs[1, 1].plot(x4, y4)
-axs[1, 1].set_title('Grain after macro fracture')
+x1, y1 = hlp.polygon_to_plot(grain.vertices[-1])
+axs.plot(x1, y1)
+
 plt.show()
