46b15407b87ee07aa287ab09410b7798dfc5fa35
lreinberg <lucasreinberg@outlook.com>
2022-03-31 11:50:56 +0200

    Various optimisations

--- Main.py
+++ Main.py
@@ -1,38 +1,46 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import grain
-import helpers as hlp
 import time
 
+import matplotlib.pyplot as plt
+import numpy as np
+
+import grain
+import helpers as hlp
 
 # Pseudo algotithm for fracture wear on a single abrasive grain (triangle)
 
 # Input parameters
-tensile_strength = 300  # 300 Mpa
-F_c = 300  # Cutting Force in Newton
-F_n = 100  # Normal Force in Newton  # Combined force vector
-d = 1  # penetration depth
+F_C = 3000  # Cutting Force in Newton
+F_N = 1000  # Normal Force in Newton
+P_DEPTH = 0.5  # penetration depth
 
 
 # Set grain vertices
-a = np.array([[0, 0], [-1, 3], [-2, 7], [2, 7]], dtype=float)
 
-#Make Grain2D Instance
-grain = grain.Grain2D(a)
+b = np.array(
+    [[-1, 0], [-3, 2], [-3, 4], [-1, 6], [1, 6], [3, 4], [3, 2], [1, 0]], dtype=float
+)
 
+# Make Grain2D Instance
+grain = grain.Grain2D(b)
 # set initial values depending on the forces and the penetration depth
-grain.initializeValues(F_c, F_n, d)
+grain.initializeValues(F_C, F_N, P_DEPTH)
 
 # Simulation
 initial_time = time.time()
-grain.make_crack(F_c, F_n)
-time1 = time.time()
-print(time1-initial_time)
+grain.make_crack(F_C, F_N)
+end_time = time.time()
+time1 = end_time - initial_time
+print(f"It took {grain.fractures} steps and {time1} seconds to crack through")
 
 # Plotting
-fig, axs = plt.subplots()
-fig.suptitle('Original and modified grain')
-x1, y1 = hlp.polygon_to_plot(grain.vertices[-1])
-axs.plot(x1, y1)
+fig, (ax1, ax2) = plt.subplots(1, 2)
+x1, y1 = hlp.polygon_to_plot(grain.vertices[0])
+x2, y2 = hlp.polygon_to_plot(grain.vertices[-1])
+ax1.plot(x1, y1)
+ax1.set_aspect("equal", "box")
+ax1.set_title("Unmodified abrasive grain", fontsize=10)
+ax2.plot(x2, y2)
+ax2.set_aspect("equal", "box")
+ax2.set_title("Abrasive grain after σ_critical < σ_result", fontsize=10)
 
 plt.show()
--- helpers.py
+++ helpers.py
@@ -1,12 +1,14 @@
 import operator
+from math import cos, sin
+
 import numpy as np
 import pyclipper
-from shapely.geometry import Polygon
-from math import cos, sin
 from matplotlib import pyplot
 from mpl_toolkits import mplot3d
+from shapely.geometry import LineString, Point, Polygon, LinearRing
 
-def get_vector_miny(array : np.ndarray) -> np.ndarray:
+
+def get_vector_miny(array: np.ndarray) -> np.ndarray:
     y = []
     for ind, value in enumerate(array):
         y.append(value[1])
@@ -14,7 +16,8 @@ def get_vector_miny(array : np.ndarray) -> np.ndarray:
     return array[y_ind]
 
 
-def get_vector_maxx(array : np.ndarray) -> np.ndarray:
+
+def get_vector_maxx(array: np.ndarray) -> np.ndarray:
     x = []
     for ind, value in enumerate(array):
         x.append(value[0])
@@ -34,12 +37,21 @@ def polygon_to_plot(polygon):
 
 
 def polygonIntersection(subj, clip):
-    '''Given 2 intersecting polygons, it returns the area of the intersection'''
+    """Given 2 intersecting polygons, it returns the area of the intersection"""
     SCALING_FACTOR = 1000
     pc = pyclipper.Pyclipper()
-    pc.AddPath(pyclipper.scale_to_clipper(subj, SCALING_FACTOR), pyclipper.PT_SUBJECT, True)
-    pc.AddPath(pyclipper.scale_to_clipper(clip, SCALING_FACTOR), pyclipper.PT_CLIP, True)
-    solution = pyclipper.scale_from_clipper(pc.Execute(pyclipper.CT_INTERSECTION, pyclipper.PFT_EVENODD, pyclipper.PFT_EVENODD), SCALING_FACTOR)
+    pc.AddPath(
+        pyclipper.scale_to_clipper(subj, SCALING_FACTOR), pyclipper.PT_SUBJECT, True
+    )
+    pc.AddPath(
+        pyclipper.scale_to_clipper(clip, SCALING_FACTOR), pyclipper.PT_CLIP, True
+    )
+    solution = pyclipper.scale_from_clipper(
+        pc.Execute(
+            pyclipper.CT_INTERSECTION, pyclipper.PFT_EVENODD, pyclipper.PFT_EVENODD
+        ),
+        SCALING_FACTOR,
+    )
     pgon = Polygon(solution[0])
     return solution[0], pgon.area
 
@@ -49,16 +61,25 @@ def getIntersectionPolygon(subj):
     subj = subj.vertices[-1]
     SCALING_FACTOR = 1000
     pc = pyclipper.Pyclipper()
-    pc.AddPath(pyclipper.scale_to_clipper(subj, SCALING_FACTOR), pyclipper.PT_SUBJECT, True)
-    pc.AddPath(pyclipper.scale_to_clipper(clip, SCALING_FACTOR), pyclipper.PT_CLIP, False)
-    solution = pyclipper.scale_from_clipper(pc.Execute(pyclipper.CT_INTERSECTION, pyclipper.PFT_EVENODD, pyclipper.PFT_EVENODD), SCALING_FACTOR)
+    pc.AddPath(
+        pyclipper.scale_to_clipper(subj, SCALING_FACTOR), pyclipper.PT_SUBJECT, True
+    )
+    pc.AddPath(
+        pyclipper.scale_to_clipper(clip, SCALING_FACTOR), pyclipper.PT_CLIP, False
+    )
+    solution = pyclipper.scale_from_clipper(
+        pc.Execute(
+            pyclipper.CT_INTERSECTION, pyclipper.PFT_EVENODD, pyclipper.PFT_EVENODD
+        ),
+        SCALING_FACTOR,
+    )
 
 
-def numpyInsertPoint(array1 : np.ndarray, array2 : np.ndarray, index : int):
-    '''python insert() but for numpy ndarray
-    
+def numpyInsertPoint(array1: np.ndarray, array2: np.ndarray, index: int):
+    """python insert() but for numpy ndarray
+
     array1 -> array in which the point is inserted
-    array2 -> point which is to be inserted'''
+    array2 -> point which is to be inserted"""
     list1 = array1.tolist()
     list2 = array2.tolist()
     list1.insert(index, list2)
@@ -66,8 +87,8 @@ def numpyInsertPoint(array1 : np.ndarray, array2 : np.ndarray, index : int):
     return arr
 
 
-def numpyInsertArrayOfPoints(array1 : np.ndarray, array2 : np.ndarray, index : int):
-    '''python insert() but for numpy ndarray'''
+def numpyInsertArrayOfPoints(array1: np.ndarray, array2: np.ndarray, index: int):
+    """python insert() but for numpy ndarray"""
     list1 = array1.tolist()
     list2 = array2.tolist()
     for ind, point in enumerate(list2):
@@ -76,8 +97,8 @@ def numpyInsertArrayOfPoints(array1 : np.ndarray, array2 : np.ndarray, index : i
     return arr
 
 
-def numpyAppend(array1 : np.ndarray, array2 : np.ndarray):
-    '''python append() but for numpy ndarray'''
+def numpyAppend(array1: np.ndarray, array2: np.ndarray):
+    """python append() but for numpy ndarray"""
     list1 = array1.tolist()
     list2 = array2.tolist()
     list1.append(list2)
@@ -85,8 +106,8 @@ def numpyAppend(array1 : np.ndarray, array2 : np.ndarray):
     return arr
 
 
-def numpyAppendArrayOfPoints(array1 : np.ndarray, array2 : np.ndarray):
-    '''python append() but for numpy ndarray'''
+def numpyAppendArrayOfPoints(array1: np.ndarray, array2: np.ndarray):
+    """python append() but for numpy ndarray"""
     list1 = array1.tolist()
     list2 = array2.tolist()
     for point in list2:
@@ -104,12 +125,15 @@ def getPointIndex(array, point):
 def mesh_rotation(stl_mesh, theta):
     """Rotates mesh by angle theta around z-axis"""
     length = len(stl_mesh.vectors)
-    rot = np.array([[cos(theta), -sin(theta), 0],
-                   [sin(theta), cos(theta), 0], [0, 0, 1]])
+    rot = np.array(
+        [[cos(theta), -sin(theta), 0], [sin(theta), cos(theta), 0], [0, 0, 1]]
+    )
     for face in range(length):
         for vertice in range(3):
             stl_mesh.vectors[face][vertice] = np.dot(
-                rot, stl_mesh.vectors[face][vertice])
+                rot, stl_mesh.vectors[face][vertice]
+            )
+    return stl_mesh
 
 
 def polygon_rotation(x, y, angle):
@@ -117,16 +141,16 @@ def polygon_rotation(x, y, angle):
     x_new = [0, 0, 0, 0]
     y_new = [0, 0, 0, 0]
     for i in range(len(x)):
-        x_new[i] = np.cos(angle) * x[i] + (-np.sin(angle))*(y[i])
-        y_new[i] = np.sin(angle)*x[i] + np.cos(angle) * (y[i])
+        x_new[i] = np.cos(angle) * x[i] + (-np.sin(angle)) * (y[i])
+        y_new[i] = np.sin(angle) * x[i] + np.cos(angle) * (y[i])
     return x_new, y_new
 
 
 def plot_mesh(mesh):
     """Plots mesh in a 3D-Plot"""
     figure = pyplot.figure()
-    axes = figure.add_subplot(projection='3d')
-    axes.add_collection3d(mplot3d.art3d.Poly3DCollection(mesh.vectors))
+    axes = figure.add_subplot(projection="3d")
+    axes.add_collection3d(mplot3d.art3d.Poly3DCollection(mesh.vectors, alpha=0.7, edgecolors='k'))
     scale = mesh.points.flatten()
     axes.auto_scale_xyz(scale, scale, scale)
     pyplot.show()
@@ -134,7 +158,7 @@ def plot_mesh(mesh):
 
 def get_angle(v1, v2):
     """Returns angle between vectors v1 and v2"""
-    return np.arccos((np.dot(v1, v2))/(np.linalg.norm(v1)*np.linalg.norm(v2)))
+    return np.arccos((np.dot(v1, v2)) / (np.linalg.norm(v1) * np.linalg.norm(v2)))
 
 
 def get_vector(start, end):
@@ -149,15 +173,58 @@ def unit_vector(vector):
     return vector / np.linalg.norm(vector)
 
 
-def rotate_point2D(point : np.ndarray, theta, rot_origin = np.array([0, 0])) -> np.ndarray:
-    rot = np.array([[np.cos(theta), -np.sin(theta)],[np.sin(theta), np.cos(theta)]])
+def rotate_point2D(point: np.ndarray, theta, rot_origin=np.array([0, 0])) -> np.ndarray:
+    rot = np.array([[np.cos(theta), -np.sin(theta)], [np.sin(theta), np.cos(theta)]])
     temp = point
     temp[0] = temp[0] - rot_origin[0]
     temp[1] = temp[1] - rot_origin[1]
     point[0] = np.cos(theta) * temp[0] + -np.sin(theta) * temp[1]
-    point[1] = np.sin(theta)*point[0] + np.cos(theta) * point[1]
+    point[1] = np.sin(theta) * point[0] + np.cos(theta) * point[1]
     point[0] = point[0] + rot_origin[0]
     point[1] = point[1] + rot_origin[1]
     return point
+
+
+def line_polygon_intersection(polygon: np.ndarray, line: np.ndarray):
+    pass
+
+
+def get_point_data(polygon, int_point):
+    point = Point(int_point)
+    for ind, test_point in enumerate(polygon):
+        line = LineString([polygon[ind - 1], test_point])
+        if line.distance(point) < 1e-8:
+            return ind - 1, polygon[ind - 1]
+
+
+def same_list_new_start(array, index):
+    temp1 = array[index:]
+    temp2 = array[:index]
+    return np.concatenate((temp1, temp2), axis=0)
+
+
+def is_clockwise(polygon : np.ndarray) -> bool:
+    ring = LinearRing(polygon)
+    if ring.is_ccw:
+        return False
+    else:
+        return True
+
+def get_cutting_edge(vertices : np.ndarray) -> np.ndarray:
+    x = []
+    y = []
+    for ind, value in enumerate(vertices):
+        y.append(value[1])
+    y_ind, y_value = min(enumerate(y), key=operator.itemgetter(1))
+    min_indexes = [i for i, z in enumerate(y) if z == y_value]
+    for ind in min_indexes:
+        x.append(vertices[ind][0])
+    x_ind, x_value = min(enumerate(x), key=operator.itemgetter(1))
+    min_ind = min_indexes[x_ind]
+    return vertices[min_ind]
+
+    
+
+
 
 
